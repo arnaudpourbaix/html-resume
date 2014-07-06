8703d2413b7612e08fc9731d174e603cf88e828f
Arnaud <arnaudpourbaix@gmail.com>
2014-07-06 18:15:39 +0200

    update logo size

--- src/app/main/services.js
+++ src/app/main/services.js
@@ -1,6 +1,37 @@
-(function mainServicesModule() {
+angular.module('onlineResume.main.services', []).service('ClientService', [ '$http', '$q', '$logger', function ClientService($http, $q, $logger) {
 	'use strict';
+	var LOGGER_NAME = module.name + '.' + this.constructor.name, log = $logger.logger(LOGGER_NAME);
 
-	var module = angular.module('onlineResume.main.services', []);
-	
-})();
+	var service = {};
+	var clientsPromise, clients;
+
+	function getClients() {
+		if (clientsPromise) {
+			return clientsPromise;
+		}
+		clientsPromise = $http({
+			method : 'GET',
+			url : 'assets/data/clients.json'
+		}).then(function(response) {
+			clients = response.data;
+			return clients;
+		});
+		return clientsPromise;
+	}
+
+	service.clients = function() {
+		return getClients();
+	};
+
+	service.client = function(id) {
+		if (!clients) {
+			throw new Error('clients are not initialized');
+		}
+		var result = _.find(clients, function(client) {
+			return client.id === id;
+		});
+		return result;
+	};
+
+	return service;
+} ]);
