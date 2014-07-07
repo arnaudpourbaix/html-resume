f875232f1dbf240632f8f9b5ac6426a37928d334
Arnaud <pourbaix.arnaud@gmail.com>
2014-07-07 14:00:30 +0200

    fix bower json add resume service update skill levels

--- src/app/main/controllers.js
+++ src/app/main/controllers.js
@@ -3,7 +3,11 @@
 
 	var module = angular.module('onlineResume.main.controllers', []);
 
-	module.controller('MainController', [ '$q', '$scope', function MainController($q, $scope) {
+	module.controller('MainController', [ '$q', '$scope', 'ResumeService', function MainController($q, $scope, ResumeService) {
+		ResumeService.resume().then(function(resume) {
+			$scope.resume = resume;
+			console.log(resume);
+		});
 	} ]);
 	
 })();
--- src/app/main/services.js
+++ src/app/main/services.js
@@ -1,36 +1,57 @@
-angular.module('onlineResume.main.services', []).service('ClientService', [ '$http', '$q', '$logger', function ClientService($http, $q, $logger) {
+angular.module('onlineResume.main.services', []).service('ResumeService', [ '$http', '$q', function ResumeService($http, $q) {
 	'use strict';
-	var LOGGER_NAME = module.name + '.' + this.constructor.name, log = $logger.logger(LOGGER_NAME);
 
 	var service = {};
-	var clientsPromise, clients;
 
-	function getClients() {
-		if (clientsPromise) {
-			return clientsPromise;
-		}
-		clientsPromise = $http({
+	function general() {
+		return $http({
 			method : 'GET',
-			url : 'assets/data/clients.json'
+			url : 'assets/data/general.json'
 		}).then(function(response) {
-			clients = response.data;
-			return clients;
+			return response.data;
 		});
-		return clientsPromise;
+	}
+	
+	function companies() {
+		return $http({
+			method : 'GET',
+			url : 'assets/data/companies.json'
+		}).then(function(response) {
+			return _.indexBy(response.data, 'id');
+		});
 	}
 
-	service.clients = function() {
-		return getClients();
-	};
-
-	service.client = function(id) {
-		if (!clients) {
-			throw new Error('clients are not initialized');
-		}
-		var result = _.find(clients, function(client) {
-			return client.id === id;
+	function skills() {
+		return $http({
+			method : 'GET',
+			url : 'assets/data/skills.json'
+		}).then(function(response) {
+			return _.indexBy(response.data, 'id');
 		});
-		return result;
+	}
+
+	function projects() {
+		return $http({
+			method : 'GET',
+			url : 'assets/data/projects.json'
+		}).then(function(response) {
+			return response.data;
+		});
+	}
+	
+	service.resume = function() {
+		var promises = [];
+		promises.push(general());
+		promises.push(companies());
+		promises.push(skills());
+		promises.push(projects());
+		return $q.all(promises).then(function(result) {
+			var resume = result[0];
+			resume.companies = result[1];
+			resume.skills = result[2];
+			resume.projects = result[3];
+			return resume;
+		});
 	};
 
 	return service;
