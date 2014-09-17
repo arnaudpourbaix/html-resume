9e471deb5fb9c4cf9a72471c4101c5c9820a0c9e
Arnaud <pourbaix.arnaud@gmail.com>
2014-09-17 14:12:11 +0200

    almostt finish testimonials clean some unused files

--- src/app/testimonial/testimonial.js
+++ src/app/testimonial/testimonial.js
@@ -6,37 +6,34 @@ angular.module('onlineResume.testimonial', [])
    	 
 })		
 
-.directive('testimonial', function($compile, $parse, $timeout) {
+.directive('testimonials', function($compile, $parse, $timeout, $interpolate) {
 	'use strict';
     return {
         restrict: 'A',
-        //template: '<span>{{skill.label}}</span><input value="{{skill.level / 20}}" class="rating">',
+        templateUrl: 'testimonial/testimonial.tpl.html',
+        replace: true,
+        scope: {
+        	testimonials : '=',
+        	companies : '='
+        },
         link: function(scope, element, attrs, controller) {
-        	var isPause = false;
-        	var pauseOnDragging = function() {
-        		isPause = true;
-        	};
-
         	$timeout(function() {
             	element.owlCarousel({
             		autoPlay : 3000,
             		stopOnHover : true,
-            		startDragging : pauseOnDragging,
             		slideSpeed  :  1000,
             		paginationSpeed : 500,
             		goToFirstSpeed : 2000,
             		singleItem : true,
             		responsive : true,
-            		// touchDrag : false,
-            		// mouseDrag : false,
             		addClassActive : true,
             		transitionStyle: 'fadeUp',
             	});
             	
-            	$('#testimonial .owl-page span').eq(0).html('<img src="assets/img/testimonial/01.png">');
-            	$('#testimonial .owl-page span').eq(1).html('<img src="assets/img/testimonial/02.png">');
-            	$('#testimonial .owl-page span').eq(2).html('<img src="assets/img/testimonial/03.png">');
-            	$('#testimonial .owl-page span').eq(3).html('<img src="assets/img/testimonial/04.png">');
+            	angular.forEach(scope.testimonials, function(testimonial, index) { // replace carousel navigation icons by photos
+            		var photo = $interpolate('<img src="assets/img/testimonial/{{filename}}">')({ filename: testimonial.photo || 'default.png' });
+            		$('#testimonial .owl-page span').eq(index).html(photo);
+            	});
         	});
         }
     };
